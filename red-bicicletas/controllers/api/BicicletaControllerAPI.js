063e57368fcdec0bacad350e9e357bf57871605b
Giacca90 <giacca90mobile@gmail.com>
2023-08-25 15:46:01 +0200

    API de bicicletas + Update

--- red-bicicletas/controllers/api/BicicletaControllerAPI.js
+++ red-bicicletas/controllers/api/BicicletaControllerAPI.js
@@ -21,3 +21,13 @@ exports.bicicleta_delete = function (req, res) {
     Bicicleta.removeById(req.body.id);
     res.status(204).send();
 }
+
+exports.bicicleta_update = function (req, res) {
+    var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
+    bici.ubicacion = [req.body.lat, req.body.lng];
+    Bicicleta.removeById(req.body.id);
+    Bicicleta.add(bici);
+    res.status(200).json({
+        bicicleta: bici
+    })
+}
