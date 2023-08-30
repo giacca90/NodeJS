9b0606723eb630f9d46ad610f0ae057481edd387
Giacca90 <giacca90mobile@gmail.com>
2023-08-30 10:11:08 +0200

    Test completo API con Jasmine

--- red-bicicletas/spec/api/bicicleta_api_test.spec.js
+++ red-bicicletas/spec/api/bicicleta_api_test.spec.js
@@ -29,4 +29,37 @@ describe('Bicicleta API', () => {
             });
         });
     });
+
+    describe('POST BICICLETA /update', () => {
+        it('STATUS 200', (done) => {
+            var headers = {'content-type' : 'application/json'};
+            var aBici = '{"id": 10, "color": "blanco", "modelo": "urbano", "lat": 41.39, "lng": 2.17}';
+            request.post({
+                headers : headers,
+                url: 'http://localhost:3000/api/bicicletas/update',
+                body: aBici
+            }, function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                expect(Bicicleta.allBicis.length).toBe(1);
+                expect(Bicicleta.findById(10).color).toBe('blanco');
+                done();
+            });
+        });
+    });
+
+    describe(' BICICLETA /delete', () => {
+        it('STATUS 400', (done) => {
+            var headers = {'content-type' : 'application/json'};
+            var nDel = '{id: 10}';
+            request.post({
+                headers : headers,
+                url: 'http://localhost:3000/api/bicicletas/delete',
+                body: nDel
+            }, function(error, response, body) {
+                expect(response.statusCode).toBe(400);
+                expect(Bicicleta.allBicis.length).toBe(0);
+                done();
+            }) 
+        })
+    })
 });
